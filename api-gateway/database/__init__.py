04d3a4749a48c2aeea1d6f77ed8da70a13278c37
Nipuna Weerasekara <w.nipuna@gmail.com>
2021-06-13 13:53:47 +0530

    Add FirebaseAuth class for implement Firebase Authentication for protected API endpoints

--- api-gateway/database/__init__.py
+++ api-gateway/database/__init__.py
@@ -1,25 +1,32 @@
 from os.path import abspath, join, dirname, realpath
-from typing import Any, Dict, Optional
 import firebase_admin
-from firebase_admin import credentials, auth
+from firebase_admin import firestore, credentials, auth
 from os import getenv
 from dotenv import load_dotenv
 from middleware.error_handling import write_log, UnauthorizedError
 
 load_dotenv()
 
-cred: Any = credentials.Certificate(
-    abspath(join(dirname(realpath(__file__)), getenv("FIREBASE_JSON")))
+cred = credentials.Certificate(
+    abspath(
+        join(dirname(dirname(realpath(__file__))), "config", getenv("FIREBASE_JSON"))
+    )
 )
 firebase_admin.initialize_app(cred, {"databaseURL": getenv("FIREBASE_DATABASE_URL")})
+firestore_db = firestore.client()
 
 
 class FirebaseAuth:
     @staticmethod
-    def check_id_token(id_token: str) -> Optional[Dict[str, str], None]:
+    def check_id_token(id_token):
         try:
             decoded_token = auth.verify_id_token(id_token)
-            return decoded_token
+            if bool(decoded_token):
+                return decoded_token.get("user_id")
+            else:
+                raise UnauthorizedError
+        except UnauthorizedError:
+            raise UnauthorizedError
         except Exception as e:
             write_log("error", e)
             raise UnauthorizedError
