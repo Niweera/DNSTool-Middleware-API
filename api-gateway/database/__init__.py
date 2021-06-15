b8e676e698a250e167f74593af21715d5875622f
Nipuna Weerasekara <w.nipuna@gmail.com>
2021-06-15 21:37:33 +0530

    Connect Firebase Authentication to create user accounts using Firebase Admin

--- api-gateway/database/__init__.py
+++ api-gateway/database/__init__.py
@@ -1,9 +1,11 @@
 from os.path import abspath, join, dirname, realpath
 from typing import Any, Dict
 import firebase_admin
-from firebase_admin import credentials, auth
+from firebase_admin import credentials, auth, db
 from os import getenv
 from dotenv import load_dotenv
+from firebase_admin.auth import UserRecord
+from firebase_admin.db import Reference
 from middleware.error_handling import write_log, UnauthorizedError
 
 load_dotenv()
@@ -16,7 +18,38 @@ cred: Any = credentials.Certificate(
 firebase_admin.initialize_app(cred, {"databaseURL": getenv("FIREBASE_DATABASE_URL")})
 
 
+class FirebaseDB:
+    def __init__(self) -> None:
+        self.root: Reference = db.reference()
+
+    def create_user_account(
+        self,
+        uid: str,
+        full_name: str,
+        email: str,
+        organization: str,
+        profession: str,
+        reason: str,
+    ) -> None:
+        try:
+            self.root.child("users").child(uid).set(
+                dict(
+                    full_name=full_name,
+                    email=email,
+                    organization=organization,
+                    profession=profession,
+                    reason=reason,
+                )
+            )
+        except Exception as e:
+            write_log("error", e)
+            raise UnauthorizedError
+
+
 class FirebaseAuth:
+    def __init__(self) -> None:
+        self.firebase_db: FirebaseDB = FirebaseDB()
+
     @staticmethod
     def check_id_token(id_token: str) -> str:
         try:
@@ -27,6 +60,29 @@ class FirebaseAuth:
                 raise UnauthorizedError
         except UnauthorizedError:
             raise UnauthorizedError
+        except Exception as e:
+            write_log("error", e)
+            raise UnauthorizedError
+
+    def register_user(
+        self,
+        full_name: str,
+        email: str,
+        organization: str,
+        profession: str,
+        reason: str,
+        password: str,
+    ) -> None:
+        try:
+            user_record: UserRecord = auth.create_user(
+                email=email,
+                email_verified=False,
+                password=password,
+                display_name=full_name,
+            )
+            self.firebase_db.create_user_account(
+                user_record.uid, full_name, email, organization, profession, reason
+            )
         except Exception as e:
             write_log("error", e)
             raise UnauthorizedError
--- api-gateway/services/services.py
+++ api-gateway/services/services.py
@@ -3,31 +3,56 @@ from os.path import abspath, join, dirname, realpath
 from typing import List, Union, Dict, Any
 from flask import Response
 from config.CustomTypes import ResourceType
+from database import FirebaseAuth, FirebaseDB
+from middleware.error_handling import write_log, NotFoundError, UnauthorizedError
 from middleware.validator import send_error
 import re
 
 
 class Service:
+    def __init__(self) -> None:
+        self.firebase_auth: FirebaseAuth = FirebaseAuth()
+        self.firebase_db: FirebaseDB = FirebaseDB()
+
     @staticmethod
     def get_root_endpoint() -> ResourceType:
         return dict(message="Root Endpoint Accessed"), 200
 
     @staticmethod
     def get_zone(query: str) -> Union[ResourceType, Response]:
-        zone_file = abspath(
-            join(dirname(dirname(realpath(__file__))), "static", "zones.json")
-        )
-        with open(file=zone_file, mode="r", encoding="utf-8") as zones_json:
-            zones: List[str] = json.load(zones_json)
-            if type(zones) != list or len(zones) == 0:
-                return send_error("error", "error occurred!", 404)
+        try:
+            zone_file = abspath(
+                join(dirname(dirname(realpath(__file__))), "static", "zones.json")
+            )
+            with open(file=zone_file, mode="r", encoding="utf-8") as zones_json:
+                zones: List[str] = json.load(zones_json)
+                if type(zones) != list or len(zones) == 0:
+                    return send_error("error", "error occurred!", 404)
 
-            lower_query: str = query.lower()
-            result: List[str] = [
-                entry for entry in zones if re.search(lower_query, entry)
-            ]
-            return dict(data=result), 200
+                lower_query: str = query.lower()
+                result: List[str] = [
+                    entry for entry in zones if re.search(lower_query, entry)
+                ]
+                return dict(data=result), 200
+        except Exception as e:
+            write_log("error", e)
+            raise NotFoundError
 
-    @staticmethod
-    def register_user(request_body: Dict[str, Any]) -> Union[ResourceType, Response]:
-        return dict(**request_body), 200
+    def register_user(
+        self, request_body: Dict[str, Any]
+    ) -> Union[ResourceType, Response]:
+        try:
+            full_name = request_body.get("full_name")
+            email = request_body.get("email")
+            organization = request_body.get("organization")
+            profession = request_body.get("profession")
+            reason = request_body.get("reason")
+            password = request_body.get("password")
+
+            self.firebase_auth.register_user(
+                full_name, email, organization, profession, reason, password
+            )
+            return dict(message="User account registered successfully"), 200
+        except Exception as e:
+            write_log("error", e)
+            raise UnauthorizedError
